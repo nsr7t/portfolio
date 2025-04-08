deda6889eb8285ad63ec74e79a4ad6effa162d81
Nasrat <nasratulla2006@gmail.com>
2025-04-08 16:26:23 +0530

    Added the tape section animation
    Signed-off-by: Nasrat <nasratulla2006@gmail.com>

--- src/sections/Tape.jsx
+++ src/sections/Tape.jsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react"
 import StarIcon from "../components/Icons/SparkleIcon"
 
 const words = [
@@ -19,14 +20,21 @@ function Tape() {
         <div className="overflow-x-clip py-16 lg:py-24">
             <div className="-mx-1 -rotate-3 bg-gradient-to-r from-emerald-300 to-sky-400">
                 <div className="mask flex">
-                    <div className="flex flex-none gap-4 py-3">
-                        {words.map((word) => (
-                            <div key={word} className="flex items-center gap-4">
-                                <span className="text-sm font-extrabold text-gray-900 uppercase">
-                                    {word}
-                                </span>
-                                <StarIcon className="size-6 -rotate-12 text-gray-900" />
-                            </div>
+                    <div className="animate-move-left flex flex-none gap-4 py-3 [animation-duration:30s]">
+                        {[...new Array(2)].fill(0).map((_, id) => (
+                            <Fragment key={id}>
+                                {words.map((word) => (
+                                    <div
+                                        key={word}
+                                        className="flex items-center gap-4"
+                                    >
+                                        <span className="text-sm font-extrabold text-gray-900 uppercase">
+                                            {word}
+                                        </span>
+                                        <StarIcon className="size-6 -rotate-12 text-gray-900" />
+                                    </div>
+                                ))}
+                            </Fragment>
                         ))}
                     </div>
                 </div>
